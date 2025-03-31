ef87709210c78b2002b4e45eacb0c76d48523365
Andrei Panteleev <andrei.panteleev@tarkov.com>
2025-03-31 10:48:27 +0300

    Add update allure-report

--- niffler-e-2-e-tests-python/utils/allure_helpers.py
+++ niffler-e-2-e-tests-python/utils/allure_helpers.py
@@ -1,58 +1,121 @@
+import allure
 import json
 import logging
+import os
 from json import JSONDecodeError
 
-import allure
 import curlify
 from allure_commons.types import AttachmentType
 from requests import Response
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def allure_attach_request(function):
     """Декоратор логироваания запроса, хедеров запроса, хедеров ответа в allure шаг и аллюр аттачмент и в консоль."""
+
     def wrapper(*args, **kwargs):
+
         method, url = args[1], args[2]
+        # self_ = args[0]
+        # headers = kwargs.get('headers', None)
+        # files = kwargs.get('files', None)
+        # data = kwargs.get('data', None)
+        # params = kwargs.get('params', None)
+        # auth = kwargs.get('auth', None)
+        # cookies = kwargs.get('cookies', None)
+        # hooks = kwargs.get('hooks', None)
+        # json_ = kwargs.get('json', None)
+        # request = Request(
+        #     method=method.upper(),
+        #     url=self_.base_url + url,
+        #     headers=headers,
+        #     files=files,
+        #     data=data or {},
+        #     json=json_,
+        #     params=params or {},
+        #     auth=auth,
+        #     cookies=cookies,
+        #     hooks=hooks,
+        # )
+        # req = PreparedRequest()
+        # req.prepare(method=request.method.upper(),
+        #     url=request.url,
+        #     files=request.files,
+        #     data=request.data,
+        #     json=request.json,
+        #     headers=merge_setting(
+        #         request.headers, self_.headers, dict_class=CaseInsensitiveDict
+        #     ),
+        #     params=merge_setting(request.params, self_.params),
+        #     auth=merge_setting(auth, self_.auth),
+        #     cookies=merge_cookies(
+        #     merge_cookies(RequestsCookieJar(), self_.cookies), cookies
+        # ),
+        #     hooks=merge_hooks(request.hooks, self_.hooks),)
+
+        # Получаем путь к директории templates относительно текущего файла
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        templates_dir = os.path.join(os.path.dirname(current_dir), 'templates')
+        
+        env = Environment(
+            loader=FileSystemLoader(templates_dir),
+            autoescape=select_autoescape()
+        )
+        template = env.get_template("http-colored-request.ftl")
+        # template = env.get_template("http-request.ftl")
+
         with allure.step(f"{method} {url}"):
 
             response: Response = function(*args, **kwargs)
-
             curl = curlify.to_curl(response.request)
-            logging.debug(curl)
-            logging.debug(response.text)
+
+            prepare_render = {
+                "request": response.request,
+                "curl": curl,
+            }
+            render = template.render(prepare_render)
 
             allure.attach(
-                body=curl.encode("utf8"),
-                name=f"Request {response.status_code}",
-                attachment_type=AttachmentType.TEXT,
-                extension=".txt"
+                body=render,
+                name=f"Request",
+                attachment_type=AttachmentType.HTML,
+                extension=".html"
             )
             try:
                 allure.attach(
                     body=json.dumps(response.json(), indent=4).encode("utf8"),
                     name=f"Response json {response.status_code}",
                     attachment_type=AttachmentType.JSON,
-                    extension=".json"
+                    extension=".html"
                 )
-            except JSONDecodeError:
+            except (JSONDecodeError, TypeError):
                 allure.attach(
                     body=response.text.encode("utf8"),
                     name=f"Response text {response.status_code}",
                     attachment_type=AttachmentType.TEXT,
                     extension=".txt")
-            allure.attach(
-                body=json.dumps(dict(response.headers), indent=4).encode("utf8"),
-                name=f"Response headers {response.status_code}",
-                attachment_type=AttachmentType.JSON,
-                extension=".json"
-            )
+            # allure.attach(
+            #     body=json.dumps(response.headers.items(), indent=4).encode("utf8"),
+            #     name=f"Response headers {response.status_code}",
+            #     attachment_type=AttachmentType.JSON,
+            #     extension=".json"
+            # )
         return response
 
     return wrapper
 
-
 def attach_sql(cursor, statement, parameters, context):
-    """Функция для прослушивания события do_execute SQLAlchemy и прикрепления SQL запроса в Allure отчет"""
-    statement_with_params = statement % parameters
-    name = statement.split(" ")[0] + " " + context.engine.url.database
-    allure.attach(statement_with_params, name=name, attachment_type=AttachmentType.TEXT)
-    return False  # Возвращаем False, чтобы SQLAlchemy продолжил выполнение запроса
+    """Функция для логирования SQL-запросов в Allure"""
+    allure.attach(
+        body=statement,
+        name="SQL Query",
+        attachment_type=AttachmentType.TEXT,
+        extension=".sql"
+    )
+    if parameters:
+        allure.attach(
+            body=str(parameters),
+            name="SQL Parameters",
+            attachment_type=AttachmentType.TEXT,
+            extension=".txt"
+        )
